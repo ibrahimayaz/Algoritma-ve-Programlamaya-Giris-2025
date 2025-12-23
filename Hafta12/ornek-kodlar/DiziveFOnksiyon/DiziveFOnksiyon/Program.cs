f61d32376905f5e5b37457e80d860e979bcbe194
ibrahimayaz <ayazibrahim13@gmail.com>
2025-12-23 16:26:09 +0300

    Hafta13 guncellendi

---
+++ Hafta12/ornek-kodlar/DiziveFOnksiyon/DiziveFOnksiyon/Program.cs
@@ -0,0 +1,12 @@
+﻿void DiziOrnegi()
+{
+    int[] sayilar = { 10, 20, 30, 40, 50 };
+
+    Console.WriteLine("\nDizi Elemanları:");
+    for (int i = 0; i < sayilar.Length; i++)
+    {
+        Console.WriteLine($"Indeks {i}: {sayilar[i]}");
+    }
+}
+//Çağırma işlemi
+DiziOrnegi();
---
+++ Hafta13/ornek-kodlar/RecursiceFonksiyonaGiris/RecursiceFonksiyonaGiris/Program.cs
@@ -0,0 +1,13 @@
+﻿int Faktoriyel(int n)
+{
+    // Taban durum
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    // Özyinelemeli adım
+    return n * Faktoriyel(n - 1);
+}
+
+Console.WriteLine(Faktoriyel(5));
