84fc9c97521ba491aed6c711c2b7b423e40e4dd1
ibrahimayaz <ayazibrahim13@gmail.com>
2025-12-30 14:32:18 +0300

    fibonacci düzenlendi

--- Hafta07/Fibonacci/Fibonacci/Program.cs
+++ Hafta07/Fibonacci/Fibonacci/Program.cs
@@ -1,14 +1,17 @@
 ﻿Console.WriteLine("Terim sayısını giriniz:");
 int n = Convert.ToInt32(Console.ReadLine());
-int a = 0;
-int b = 1;
+
+int n_2 = 0; // F(n-2)
+int n_1 = 1; // F(n-1)
+
 int i = 1;
 while (i <= n)
 {
-    Console.Write(a + " ");
-    var temp = a + b;
-    a = b;
-    b= temp;
-    i++;
+    Console.Write(n_2 + " ");
 
+    int n = n_2 + n_1; // F(n)
+    n_2 = n_1;          // yeni F(n-2)
+    n_1 = n;            // yeni F(n-1)
+
+    i++;
 }
